e86c463332a35e3d94ca5c83b6add6368c79f30c
Codi Kraus <codi.kraus@gmail.com>
2021-05-05 17:06:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,6 +45,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     season.each do |attribute, value|
       puts "#{attribute}: #{value}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
