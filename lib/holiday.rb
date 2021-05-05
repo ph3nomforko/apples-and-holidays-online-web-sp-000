41301da3dcea43fee3e310f06b91c936aae5d633
Codi Kraus <codi.kraus@gmail.com>
2021-05-05 17:24:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |attribute, value|
       if value.include?("BBQ")
-        return_array.push(value)
+        return_array.push(attribute)
       end
     end
   end
